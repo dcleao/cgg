375acf3b99aa82c09f7324e1a36a3de4f1e93bf9
Duarte Cunha Leão <duarte.leao@gmail.com>
2019-11-27 01:06:07 +0000

    [BACKLOG-31898] Changing AMD id of CSRF token service. Sync AMD config with Common-UI

--- core/src/main/javascript/pt/webdetails/cgg/resources/define-cfg.js
+++ core/src/main/javascript/pt/webdetails/cgg/resources/define-cfg.js
@@ -552,7 +552,6 @@
         "pentaho/theme/impl/Service",
         "pentaho/theme/service",
         "pentaho/theme/LoadThemeAnnotation",
-        "pentaho/csrf/service",
         "pentaho/lang/UserError",
         "pentaho/lang/RuntimeError",
         "pentaho/lang/Event",
